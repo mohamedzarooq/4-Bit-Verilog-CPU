8e8e69cb2e8a665b10135a3a997cc1d5ce647106
Mohamed <mohamedzarooq07@gmail.com>
2026-08-26 14:03:12 -0600

    Added comp_zero to be displayed when VVP file runs to verify flag works correctly

--- tb/cpu_tb.v
+++ tb/cpu_tb.v
@@ -19,11 +19,11 @@
         #10;
         reset = 0;
     
-        repeat (30) begin
+        repeat (50) begin
             @(posedge clk);
             #1;
     
-            $display("PC = %0d | instr = %b | R0 = %0d | zero = %b", uut.pc_out, uut.instr, uut.r0, uut.zero_flag);
+            $display("PC = %0d | instr = %b | R0 = %0d | zero_flag = %b | zero_comp = %b", uut.pc_out, uut.instr, uut.r0, uut.zero_flag, uut.comp_zero);
         end
     
         $finish;
